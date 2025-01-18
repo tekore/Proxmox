3bfca2b6ebbb81abf4350e787f8ca43d09c0bd53
tekore <tekore@tuta.com>
2025-01-18 20:26:45 +0100

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,9 @@ terraform {
 }
 
 provider "proxmox" {
-  insecure = true
   ssh {
-    agent = true
+    agent = false
+    username = var.PROXMOX_VE_USERNAME
+    private_key = file("~/.ssh/id_rsa") # cat this into a file and use it here in the pipeline
   }
 }
