d31e28116fe4fecbe13a5af2be690fefda5f2eb8
tekore <tekore@tuta.com>
2025-01-18 20:34:58 +0100

    updates

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,1 +1,11 @@
+variable "proxmox_username" {
+   description = "Proxmox API Username"
+   type        = string
+   sensitive   = true
+}
 
+variable "proxmox_private_key" {
+   description = "Proxmox API Key"
+   type        = string
+   sensitive   = true
+}
